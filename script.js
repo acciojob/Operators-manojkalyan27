27179be3ca095d462bbb84cd3fbfa6db27892b5b
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-03 15:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const changeText = document.querySelector("#change_text");
+
+changeText.addEventListener('click',()=>{
+    const originalText = document.querySelectorAll(".division");
+    let arr = ['Bye','Good Bye','To','Your','Class'];
+    originalText.forEach((item,index)=>{
+        item.textContent = arr[index];
+    })    
+});
+
+
+const operation = document.querySelector('#calculate');
+
+operation.addEventListener('click',()=>{
+    const num1 = parseInt(document.querySelector('#num1').value);
+    const num2 = parseInt(document.querySelector('#num2').value);
+    const operator = document.getElementById("operator").value;
+
+    let result;
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num2 !== 0 ? num1 / num2 : "Error: Division by zero";
+    } else {
+      result = "Invalid operator";
+    }
+
+    document.getElementById("result").innerText = `Result: ${result}`;
+})
